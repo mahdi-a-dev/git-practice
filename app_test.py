c66da1b8ea84d1271e8c8f38f9569eacbc3fcd5f
mohammadreza <mahdi.a.dev.work@gmail.com>
2026-08-08 21:38:13 +0330

    completed app_test.py , add test for app.py functions

--- app_test.py
+++ app_test.py
@@ -1,13 +1,64 @@
 from app import *
+from logger import *
 
 # add
-print(add(4, 5))
+def test_add(a, b, r):
+    log("starting testing add function")
+    result = add(a, b)
+    if result == r:
+        log("test successful")
+        success_message(f"adding {a} + {b} is equal to {r}")
+    else:
+        error("test failed")
+        failed_message(f"adding {a} + {b} is not equal to {r}")
+    log("test ended.")
+
+test_add(7, 4, 11)
+
+print("-------------------------------------------")
 
 # subtract
-print(subtract(100, 45))
+def test_subtract(a, b, r):
+    log("starting testing subtract function")
+    result = subtract(a, b)
+    if result == r:
+        log("test successful")
+        success_message(f"subtracting {a} - {b} is equal to {r}")
+    else:
+        error("test failed")
+        failed_message(f"subtracting {a} - {b} is not equal to {r}")
+    log("test ended.")
+
+test_subtract(50, 15, 35)
+
+print("-------------------------------------------")
 
 # multiply
-print(multiply(5, 5))
+def test_multiply(a, b, r):
+    log("starting testing multiply function")
+    result = multiply(a, b)
+    if result == r:
+        log("test successful")
+        success_message(f"multipling {a} * {b} is equal to {r}")
+    else:
+        error("test failed")
+        failed_message(f"multipling {a} * {b} is not equal to {r}")
+    log("test ended.")
+
+test_multiply(5, 5, 25)
+
+print("-------------------------------------------")
 
 # division
-print(division(10, 5))
+def test_division(a, b, r):
+    log("starting testing division function")
+    result = division(a, b)
+    if result == r:
+        log("test successful")
+        success_message(f"dividing {a} / {b} is equal to {r}")
+    else:
+        error("test failed")
+        failed_message(f"dividing {a} / {b} is not equal to {r}")
+    log("test ended.")
+
+test_division(100, 5, 20)
