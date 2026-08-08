b8deb4ab3cb508ae37c40292cc98f59316c2f706
mohammadreza <mahdi.a.dev.work@gmail.com>
2026-08-08 21:05:31 +0330

    Add success and failed messages

--- logger.py
+++ logger.py
@@ -3,3 +3,9 @@ def log(message):
 
 def error(message):
     print(f"[ERROR] {message}")
+
+def success_message(message):
+    print(f"[SUCCESS] {message}")
+
+def failed_message(message):
+    print(f"[FAILED] {message}")
