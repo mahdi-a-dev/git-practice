f0f79849e14308989a0eccb8292a49ec4e732fa7
mohammadreza <mahdi.a.dev.work@gmail.com>
2026-08-13 18:33:03 +0330

    Add python practice season 2

---
+++ python-practice/s2-practice.py
@@ -0,0 +1,56 @@
+# 1
+print("-----------1-----------")
+x = 5
+y = 10
+x, y = y, x
+
+print("x:",x)
+print("y:",y)
+
+# 2
+print("-----------2-----------")
+length = 25
+width = 15
+area = length * width
+print(f"area: {area}")
+
+# 3
+print("-----------3-----------")
+celsius = 15
+fahrenheit = (celsius * 1.8) + 32
+print(f"fahrenheit: {fahrenheit}")
+
+# 4
+print("-----------4-----------")
+radius = 5
+p = 3.14
+print(f"aria: {p * radius * radius}\nperimeter: {(2 * p * radius):.2f} ")
+
+# 5
+print("-----------5-----------")
+x = 5
+y = 3
+add = x + y
+sub = x - y
+multi = x * y
+pow = x ** y
+print(f"x: {x}\ny: {y}\nadd: {add}\nsub: {sub}\nmulti: {multi}\npow: {pow}")
+
+# 6
+print("-----------6-----------")
+x = 15
+y = 16
+z = 20
+print(f"avg: {(x + y + z) / 3}")
+
+# 7
+print("-----------7-----------")
+heigth = 15
+base = 20
+print(f"rectangle area: {2 / (base * heigth)}")
+
+# 8
+print("-----------8-----------")
+age = 20
+year = 2026
+print(f"you will be 100 years old in {year + (100 - age)}")
